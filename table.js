46f81dde86d768cf2c9c589093a3f8b70c04c14c
locodiosa <locodiosa@mail.ru>
2019-09-12 18:20:12 +0300

    Draw Table, Shade on mouseover, Coin on click (anywhere on canvas)

--- table.js
+++ table.js
@@ -1,22 +1,63 @@
 "use strict";
 
-let mainloop = function() {
-	draw();
+let clientWidth = document.documentElement.clientWidth;
+let clientHeight = document.documentElement.clientHeight;
+let scale = (clientWidth <= clientHeight) ? clientWidth * 0.9 : clientHeight * 0.9;
+
+let canvas = document.getElementById("canvas");
+canvas.width  = scale; 
+canvas.height = scale;
+
+let context = canvas.getContext('2d');
+
+let table = {
+	radius: scale / 2 * 0.9,
+	draw: function(context) {
+		context.fillStyle = '#888'; 
+		context.arc(scale / 2, scale / 2, this.radius, 0, 2 * Math.PI, true);		
+	}
 }
 
-let FRAME_RATE_HZ = 50;
+let coin = {	
+	radius: table.radius * 0.07,	
+	draw: function(context, x, y) {
+		context.fillStyle = '#008';
+		context.arc(x, y, this.radius, 0, 2 * Math.PI, true);		
+	}	
+};
 
-setInterval(mainloop, 1000 / FRAME_RATE_HZ);
+drowTable(context);
 
-function draw() {
-	let canvas  = document.getElementById("canvas");
-	canvas.width  = 600; 
-	canvas.height = 600; 
-	canvas.radius  = 200;
-		
-	let context = canvas.getContext('2d');
-	context.fillStyle = '#000'; 
-	context.arc(250, 250, canvas.radius, 0, 2 * Math.PI, true);		
+canvas.addEventListener("mouseover", e => {
+	let x = e.offsetX;
+	let y = e.offsetY;	    
+    drowShade(context, x, y);
+});
+
+canvas.addEventListener("click", e => {
+	console.log(e.offsetX + ", " + e.offsetY);
+	let x = e.offsetX;
+	let y = e.offsetY;	    
+    drowCoin(context, x, y);
+});
+
+function drowTable(context) {	
+	context.beginPath();
+	table.draw(context);
+	context.closePath();
+	context.fill();		
+}
+
+function drowCoin(context, x, y) {	
+	context.beginPath();
+	coin.draw(context, x, y);
+	context.closePath();
 	context.fill();	
 }
 
+function drowShade(context, x, y) {	
+	context.beginPath();
+	coin.draw(context, x, y);
+	context.closePath();
+	context.stroke();		
+}
