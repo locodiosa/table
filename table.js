5b830a2eca178b995f4e94bc963f2de886719e72
locodiosa <locodiosa@mail.ru>
2019-09-17 19:58:35 +0300

    Game is realized

--- table.js
+++ table.js
@@ -10,6 +10,7 @@ canvas.height = scale;
 let center = scale / 2;
 
 let context = canvas.getContext('2d');
+let times = 0;
 
 let table = {
 	radius: center * 0.9,
@@ -20,12 +21,14 @@ let table = {
 }
 
 let coin = {	
-	radius: table.radius * 0.07,	
-	draw: function(context, x, y) {
-		context.fillStyle = '#008';
+	radius: table.radius * 0.1,	
+	draw: function(context, x, y, color) {
+		context.fillStyle = color;
 		context.arc(x, y, this.radius, 0, 2 * Math.PI, true);		
 	}	
 };
+
+let allCoints = [];
 
 drowTable(context);
 
@@ -33,21 +36,28 @@ canvas.addEventListener("click", e => {
 	let x = e.offsetX;
 	let y = e.offsetY;
 
-	if (Math.sqrt(Math.pow(center - y, 2) + Math.pow(center - x, 2)) 
-		<= table.radius - coin.radius) {
-		drowShade(context, x, y);
-	} 
-});
-
-canvas.addEventListener("dblclick", e => {
-	let x = e.offsetX;
-	let y = e.offsetY;	
-
-    if (Math.sqrt(Math.pow(center - y, 2) + Math.pow(center - x, 2)) 
-		<= table.radius - coin.radius) {
-		drowCoin(context, x, y);
+    if (getTableArea(x, y) && (allCoints.length == 0 || checkNeighbours(x, y))) {
+    	++times;
+    	drowCoin(context, x, y, chooseColor(times));
+    	allCoints.push([x, y]);
 	}
 });
+
+function checkNeighbours(x, y) {
+	let neighbours = allCoints.filter(c => Math.sqrt(Math.pow(c[1] - y, 2) + Math.pow(c[0] - x, 2)) 
+			< coin.radius * 2);
+
+	return (neighbours.length > 0) ? false : true;
+}
+
+function getTableArea(x, y) {
+	return (Math.sqrt(Math.pow(center - y, 2) + Math.pow(center - x, 2)) 
+			<= table.radius - coin.radius) ? true : false;
+}
+
+function chooseColor(times) {
+	return (times % 2 == 0) ? "#008" : "#800";
+}
 
 function drowTable(context) {	
 	context.beginPath();
@@ -56,9 +66,9 @@ function drowTable(context) {
 	context.fill();		
 }
 
-function drowCoin(context, x, y) {	
+function drowCoin(context, x, y, color) {	
 	context.beginPath();
-	coin.draw(context, x, y);
+	coin.draw(context, x, y, color);
 	context.closePath();
 	context.fill();	
 }
