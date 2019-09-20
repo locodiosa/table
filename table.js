92844599374629267f7a31e4417a140869317b2e
locodiosa <locodiosa@mail.ru>
2019-09-20 17:51:10 +0300

    Draw shade

--- table.js
+++ table.js
@@ -9,13 +9,14 @@ canvas.width  = scale;
 canvas.height = scale;
 let center = scale / 2;
 
-let context = canvas.getContext('2d');
+let context = canvas.getContext("2d");
 let times = 0;
+let shade = 0;
 
 let table = {
 	radius: center * 0.9,
 	draw: function(context) {
-		context.fillStyle = '#888'; 
+		context.fillStyle = "#888"; 
 		context.arc(center, center, this.radius, 0, 2 * Math.PI, true);		
 	}
 }
@@ -24,6 +25,7 @@ let coin = {
 	radius: table.radius * 0.1,	
 	draw: function(context, x, y, color) {
 		context.fillStyle = color;
+		context.strokeStyle = color;
 		context.arc(x, y, this.radius, 0, 2 * Math.PI, true);		
 	}	
 };
@@ -42,6 +44,29 @@ canvas.addEventListener("click", e => {
     	allCoints.push([x, y]);
 	}
 });
+
+let oldX = null;
+let oldY = null;
+
+canvas.addEventListener("mousemove", e => {
+	let x = e.offsetX;
+	let y = e.offsetY;
+
+    if (getTableArea(x, y) && (allCoints.length == 0 || checkNeighbours(x, y))) {
+    	if (shade == 1) {
+    		drowShade(context, oldX, oldY, "#888");
+    		shade = 0;
+    	}
+
+    	if (shade == 0) {
+    		drowShade(context, x, y, "#000");
+    		shade = 1;
+    		oldX = x;
+    		oldY = y;    		
+    	}    	
+	}
+});
+
 
 function checkNeighbours(x, y) {
 	let neighbours = allCoints.filter(c => Math.sqrt(Math.pow(c[1] - y, 2) + Math.pow(c[0] - x, 2)) 
@@ -73,9 +98,9 @@ function drowCoin(context, x, y, color) {
 	context.fill();	
 }
 
-function drowShade(context, x, y) {	
+function drowShade(context, x, y, color) {	
 	context.beginPath();
-	coin.draw(context, x, y);
+	coin.draw(context, x, y, color);
 	context.closePath();
 	context.stroke();		
 }
